b122441f8aa6e9c1ae9b1ab3860f3d553e574ac5
KylanTraynor <kylan2837@gmail.com>
2016-11-11 20:37:31 +0100

    Initial Commit

---
+++ DraggyData/src/main/java/com/kylantraynor/DraggyData/DraggyData.java
@@ -0,0 +1,13 @@
+package com.kylantraynor.DraggyData;
+
+public class DraggyData extends JavaPlugin{
+	@Override
+	public void onEnable(){
+		
+	}
+	
+	@Override
+	public void onDisable(){
+		
+	}
+}
