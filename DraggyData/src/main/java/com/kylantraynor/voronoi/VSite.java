b4ee52a7a23902ca050417f83407cc01c14de61d
KylanTraynor <kylan2837@gmail.com>
2017-07-15 18:52:36 +0200

    Changed some hashcodes.

--- DraggyData/src/main/java/com/kylantraynor/voronoi/VSite.java
+++ DraggyData/src/main/java/com/kylantraynor/voronoi/VSite.java
@@ -13,4 +13,24 @@ public class VSite extends VectorXZ{
 	public double weightedDistance(VectorXZ v){
 		return Math.sqrt( (x-v.x)*(x-v.x) + (z-v.z)*(z-v.z) ) * weight;
 	}
+	
+	@Override
+	public boolean equals(Object o){
+		if(o instanceof VSite){
+			if(o.hashCode() == this.hashCode())
+				if(x == ((VSite)o).x && z == ((VSite)o).z && weight == ((VSite)o).weight){
+					return true;
+				}
+		}
+		return false;
+	}
+	
+	@Override
+	public int hashCode(){
+		int hash = 13;
+		hash = hash * Float.hashCode(x);
+		hash = hash * Float.hashCode(z);
+		hash = hash * Float.hashCode(weight);
+		return hash;
+	}
 }
